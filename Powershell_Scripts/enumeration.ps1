2099ae07183136d5c3c66fd6deec3989bdfa1181
hackersupcoming <168341812+hackersupcoming@users.noreply.github.com>
2024-06-21 22:52:39 +0530

    Update enumeration.ps1

--- Powershell_Scripts/enumeration.ps1
+++ Powershell_Scripts/enumeration.ps1
@@ -10,3 +10,11 @@ $PDC = $domainObj.PdcRoleOwner.Name
 
 # Print the PdcRoleOwner name
 Write-Host "PdcRoleOwner: $PDC"
+
+# Print the distinguished name of the domain
+$distinguishedName = ([adsi]'').distinguishedName
+Write-Host "Distinguished Name: $distinguishedName"
+
+# Create the full LDAP path
+$ldapPath = "LDAP://" + $distinguishedName
+Write-Host "Full LDAP Path: $ldapPath"
