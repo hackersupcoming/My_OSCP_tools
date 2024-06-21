c1bedf23ff293997d8a7bf751b008150716034c7
hackersupcoming <168341812+hackersupcoming@users.noreply.github.com>
2024-06-21 22:46:45 +0530

    Update enumeration.ps1

--- Powershell_Scripts/enumeration.ps1
+++ Powershell_Scripts/enumeration.ps1
@@ -1,6 +1,8 @@
-
 # Store the domain object in the $domainObj variable
 $domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
 
-# Print the variable
-$domainObj
+# Store the PdcRoleOwner name to the $PDC variable
+$PDC = $domainObj.PdcRoleOwner.Name
+
+# Print the $PDC variable
+$PDC
